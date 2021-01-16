70b4571b3fd8ccfd3ee05a8936947a52e5729896
Veda <vedac72@gmail.com>
2021-01-16 17:17:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/EjercicioClase15121.kt
@@ -0,0 +1,44 @@
+package com.example
+
+//Ejercicio 1
+/*
+fun main (){
+    var number: Int = 14
+    val name = "Ejercicio calse 1 Módulo 4"
+
+    println("El valor del número es: $number y el nombre es: $name")
+}*/
+
+//Ejercicio 2
+
+fun main(){
+    var number: Int = 9
+    val name= "Veda"
+    var char= "VedaC"
+    var String= "VC"
+
+    println(number)
+    println(name)
+    println(char)
+    println(String)
+    println("El nombre es $name y las iniciales son $String")
+}
+
+//Ejercicio 3
+/*
+fun main(){
+    var number: Int = 10
+    var number2: Int = 33
+    var number3: Int = 66
+
+    var number4: Int = number+number2+number3
+    println("El resultado es: $number4")
+
+    number = 55
+    number4 = number+number2+number3
+    println("El resultado es: $number4")
+
+    var total: Int = number4/3
+    println("El resultado es: $total")
+
+}*/
